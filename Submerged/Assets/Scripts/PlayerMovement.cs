9d82a866e2f8d4a627ecbf0e91176ad32f3b66c2
Jackson <jvs8230@rit.edu>
2021-11-17 01:58:03 -0500

    added a grappling hook and fixed the movement and animation

--- Submerged/Assets/Scripts/PlayerMovement.cs
+++ Submerged/Assets/Scripts/PlayerMovement.cs
@@ -18,14 +18,15 @@ public class PlayerMovement : MonoBehaviour
     private Animator animator;
     private SpriteRenderer spriteRenderer;
     private float moveInput;
-    
+
     public float speed;
+    public float acceleration;
     public float jumpForce;
     public Vector2 moveAmount = Vector2.zero;
     public MovementState currentMovState;
 
     //Jumping
-    private bool isGrounded;
+    public bool isGrounded;
     public Transform groundCheck;
     public float checkRadius;
     public LayerMask whatIsGround;
@@ -43,23 +44,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D) && isGrounded)
+        {
+            currentMovState = MovementState.Idle;
+        }
+        else if (Input.GetKey(KeyCode.A))
         {
             currentMovState = MovementState.Left;
         }
-        else if(Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             currentMovState = MovementState.Right;
         }
-        else if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            currentMovState = MovementState.Jump;
-            playerBody.velocity = Vector2.up * jumpForce;
-        }
-        
         else
         {
             currentMovState = MovementState.Idle;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            currentMovState = MovementState.Jump;
+            playerBody.velocity = Vector2.up * jumpForce;
         }
 
         if (!isGrounded)
@@ -106,12 +111,23 @@ public class PlayerMovement : MonoBehaviour
 
         //Handles movement
         moveInput = Input.GetAxisRaw("Horizontal");
-        playerBody.velocity = new Vector2(moveInput * speed, playerBody.velocity.y);
+        float delta = 0;
+        if (moveInput > 0)
+        {
+            delta = Mathf.Max(0, Time.deltaTime * acceleration * (speed - playerBody.velocity.x));
+        }
+        if (moveInput < 0)
+        {
+            delta = Mathf.Min(0, Time.deltaTime * acceleration * (-speed - playerBody.velocity.x));
+        }
+        Vector2 walkDelta = new Vector2(delta, 0);
+        playerBody.velocity = playerBody.velocity + walkDelta;
 
-        if (Mathf.Approximately(playerBody.velocity.x, 0.0f))
-            animator.SetFloat("Speed", 0.0f);
+        //if (Mathf.Approximately(playerBody.velocity.x, 0.0f))
+        //  animator.SetFloat("Speed", 0.0f);
 
-        else
-            animator.SetFloat("Speed", playerBody.velocity.x);
+        //else
+        // animator.SetFloat("Speed", playerBody.velocity.x);
     }
+
 }
