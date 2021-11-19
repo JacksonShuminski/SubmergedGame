1ee67cd6a2a63ba1d92a26e0830eb3f6e1f8f823
Jackson <jvs8230@rit.edu>
2021-11-19 01:09:16 -0500

    Got the moving block in

---
+++ Submerged/Assets/Scripts/PathObject.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathObject : MonoBehaviour
+{
+    public Transform[] nodes;
+    public float radius;
+    public float speed;
+    public int current;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;
+        if (nodes.Length > 0)
+        {
+            transform.position = nodes[0].position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 toNode = nodes[(current + 1) % nodes.Length].position - transform.position;
+        toNode.z = 0;
+        if (toNode.magnitude > speed*Time.deltaTime)
+        {
+            transform.position += toNode.normalized * speed * Time.deltaTime;
+        }
+        else
+        {
+            current++;
+        }
+    }
+}
