6b9dd6df4faafaedb2c1389d721ecc2c651dd50f
Tanuki3017 <57275147+Tanuki3017@users.noreply.github.com>
2021-11-21 17:45:18 -0500

    Camera follows player

--- Submerged/Assets/Scripts/PlayerCamera.cs
+++ Submerged/Assets/Scripts/PlayerCamera.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public Transform target;
+    public float smoothTime;
+    private Vector3 velocity;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 targetPosition = target.TransformPoint(0, 0, -10);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
