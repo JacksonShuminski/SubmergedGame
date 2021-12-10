fff6647d975585f13941ee9c6cf62c9a90246b5f
Zach Selsley (RIT Student) <zds8702@rit.edu>
2021-12-09 21:00:24 -0500

    Zipped the build

---
+++ Submerged Build/Submerged_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,79 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID DEATH = 779278001U;
+        static const AkUniqueID FINALVICTORY = 302641207U;
+        static const AkUniqueID GAMEOVER = 4158285989U;
+        static const AkUniqueID GAMEWIN = 1776280627U;
+        static const AkUniqueID LEVEL1 = 2678230382U;
+        static const AkUniqueID LEVEL2 = 2678230381U;
+        static const AkUniqueID LEVEL3 = 2678230380U;
+        static const AkUniqueID TITLE = 3705726509U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace ACTUALGAME
+        {
+            static const AkUniqueID GROUP = 3442759021U;
+
+            namespace STATE
+            {
+                static const AkUniqueID DEFEAT = 1593864692U;
+                static const AkUniqueID FINALVICTORY = 302641207U;
+                static const AkUniqueID LEVEL_1 = 1290008369U;
+                static const AkUniqueID LEVEL_2 = 1290008370U;
+                static const AkUniqueID LEVEL_3 = 1290008371U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TITLE = 3705726509U;
+                static const AkUniqueID VICTORY = 2716678721U;
+            } // namespace STATE
+        } // namespace ACTUALGAME
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace GAME
+        {
+            static const AkUniqueID GROUP = 702482391U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace GAME
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SUBMERGED = 2530502021U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
